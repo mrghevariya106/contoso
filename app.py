3b29a1c8e60f05e55b3fb5b2d29f45a2f3c91db2
MAYUR JITUBHAI GHEVARIYA <50697840+mrghevariya106@users.noreply.github.com>
2022-01-22 15:43:12 +0530

    Final Commit

--- app.py
+++ app.py
@@ -12,49 +12,47 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/',methods=['POST'])
+@app.route('/', methods=['POST'])
 def index_post():
-    # read the value from the form
+    # Read the values from the form
     original_text = request.form['text']
-    target_languages = request.form['languages']
-    
-    #Load the value from the form .env
+    target_language = request.form['language']
+
+    # Load the values from .env
     key = os.environ['KEY']
     endpoint = os.environ['ENDPOINT']
     location = os.environ['LOCATION']
-    
+
     # Indicate that we want to translate and the API version (3.0) and the target language
     path = '/translate?api-version=3.0'
-    
     # Add the target language parameter
-    target_languages_parameter = '&to=' +target_languages
-    
+    target_language_parameter = '&to=' + target_language
     # Create the full URL
-    constructed_url = endpoint + path + target_languages_parameter
-    
-    # Set up the header information,which includes our subscription key
+    constructed_url = endpoint + path + target_language_parameter
+
+    # Set up the header information, which includes our subscription key
     headers = {
         'Ocp-Apim-Subscription-Key': key,
         'Ocp-Apim-Subscription-Region': location,
         'Content-type': 'application/json',
-        'X-ClientTraceId' : str(uuid.uuid4())
+        'X-ClientTraceId': str(uuid.uuid4())
     }
-    
-    # Create the bosy of the request with the text to be translated
-    body = [{ 'text' : original_text }]
-    
-    # Make the call using POST
+
+    # Create the body of the request with the text to be translated
+    body = [{ 'text': original_text }]
+
+    # Make the call using post
     translator_request = requests.post(constructed_url, headers=headers, json=body)
     # Retrieve the JSON response
     translator_response = translator_request.json()
     # Retrieve the translation
     translated_text = translator_response[0]['translations'][0]['text']
-    
+
     # Call render template, passing the translated text,
     # original text, and target language to the template
     return render_template(
         'results.html',
         translated_text=translated_text,
         original_text=original_text,
-        target_languages = target_languages
+        target_language=target_language
     )
